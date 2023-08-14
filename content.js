ad09293ad1431158c8998aeca27371bc4569ba10
Matthew McNee <matthew.mcnee@student.manchester.ac.uk>
2023-08-14 15:13:28 +0100

    added iframe changes

--- content.js
+++ content.js
@@ -38,9 +38,7 @@ var inlineCSS = `
 
 .stream_header{
   background-color: #fff !important;
-/*    background-image: none !important;
-  background-repeat: initial !important;*/
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2) !important;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2) !important;
   z-index: 6 !important;
   position: relative;
 }
@@ -52,19 +50,19 @@ var inlineCSS = `
 
 .left_stream_wrapper{
 /*  box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2) !important;*/
-border: none !important;
-background-color: #fff !important;
-z-index: 5 !important;
-box-shadow: none !important;
+  border: none !important;
+  background-color: #fff !important;
+  z-index: 5 !important;
+  box-shadow: none !important;
 }
 
 .ui-datepicker-calendar .ui-state-default{
   width: 30px !important;
   height: 20px !important;
   text-align: center !important;
-    border: none !important;
-    border-radius: 5px !important;
-    background-color: #f0f0f0 !important;
+  border: none !important;
+  border-radius: 5px !important;
+  background-color: #f0f0f0 !important;
 
 }
 
@@ -72,8 +70,104 @@ box-shadow: none !important;
   padding: 3px 0 !important;
 }
 
+/*.ui-datepicker-calendar table {
+    border-spacing: 2px;
+    row-gap: 2px !important;
+}*/
+
 .fc-border-separate .fc-state-highlight{
   background-color: lightblue !important;
+}
+
+.calendar-item .label-color{
+  border-radius: 6px !important;
+  width: 20px !important;
+  height: 20px !important;
+  padding: 0 !important;
+  margin-top: 2px !important;
+  margin-bottom: 2px !important;
+}
+.calendar-item input[type="checkbox"]{
+  appearance: none;
+  background-color: #eee;
+  width: 10px;
+  height: 10px;
+  margin-top: 4.8px !important;
+  margin-left: 4.8px !important;
+  border-radius: 2px !important;
+  cursor: pointer;
+}
+
+.calendar-item input[type="checkbox"]:after{
+  content: '';
+  position: absolute;
+  background-color: #eee;
+  width: 6px;
+  height: 6px;
+  top: 2px !important;
+  left: 2px !important;
+  border-radius: 1px !important;
+  pointer-events: none;
+  cursor: pointer;
+}
+
+.calendar-item input:checked[type="checkbox"]:after{
+  background-color: #333;
+}
+
+.calendar-item label{
+  border: none !important;
+  padding-right: 20px !important;
+}
+
+.calendar-item .notch{
+  display: none !important;
+}
+
+.colorPicker-palette{
+  background-color: #fff !important;
+}
+
+.colorPicker-picker{
+  position: absolute;
+  right: 0 !important;
+  top: -4px !important; 
+  transform: rotateZ(45deg) scale(1.2);
+}
+
+#checkUncheckAll{
+  margin-bottom: 10px !important;
+}
+
+#checkUncheckAll a{
+  text-decoration: none !important;
+  outline: none !important;
+}
+
+#ical{
+  background-color: #5E0366 !important;
+  background-image: none !important;
+  box-shadow: inset 0 0 0 2px #5E0366 !important;
+  border: none !important;
+  border-radius: 5px !important;
+  text-align: center;
+  padding: 0.5625rem 1.125rem 0.625rem !important;
+  text-shadow: none !important;
+  color: white !important;
+}
+
+#ical:hover{
+  color: #eee !important;
+}
+
+.fc-header button{
+  background-image: none !important;
+  background-color: #dadada;
+  box-shadow: inset 0 0 0 2px #dadada;
+  border: none !important;
+  border-radius: 5px !important;
+  text-align: center;
+  padding: 0.5625rem 1.125rem 0.625rem !important;
 }
 
 
