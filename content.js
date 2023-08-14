c12c1b2ba43a49c177110c0a54b1ef7d09031bf9
Matthew McNee <matthew.mcnee@student.manchester.ac.uk>
2023-08-14 09:43:07 +0100

    Can now edit the embedded content

--- content.js
+++ content.js
@@ -14,58 +14,73 @@ console.log('Custom script injected');
 
 // const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 // console.log(iframeDoc);
+       // Function to add inline CSS rules inside the iframe
+function addInlineStylesInIframe(iframeElement, cssRules) {
+    iframeElement.onload = function() {
+        var iframeDocument = iframeElement.contentDocument || iframeElement.contentWindow.document;
+        console.log(iframeDocument);
+        // Create a new <style> element for inline styles
+        var styleElement = iframeDocument.createElement("style");
+        styleElement.type = "text/css";
+
+        console.log(cssRules);
+        styleElement.appendChild(iframeDocument.createTextNode(cssRules)); // For modern browsers
+        
+        console.log(styleElement);
+        // Append the style element to the iframe's head
+        iframeDocument.head.appendChild(styleElement);
+    };
+}
+
+        // Call the function to add inline styles
+var iframe = document.getElementById("mybbCanvas");
+var inlineCSS = `
+
+.stream_header{
+  background-color: #fff !important;
+/*    background-image: none !important;
+  background-repeat: initial !important;*/
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2) !important;
+  z-index: 6 !important;
+  position: relative;
+}
+
+.stream_page_left {
+  border: none !important;
+
+}
+
+.left_stream_wrapper{
+/*  box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2) !important;*/
+border: none !important;
+background-color: #fff !important;
+z-index: 5 !important;
+box-shadow: none !important;
+}
+
+.ui-datepicker-calendar .ui-state-default{
+  width: 30px !important;
+  height: 20px !important;
+  text-align: center !important;
+    border: none !important;
+    border-radius: 5px !important;
+    background-color: #f0f0f0 !important;
+
+}
+
+.ui-datepicker-calendar td{
+  padding: 3px 0 !important;
+}
+
+`;
 
 
 
-        function loadStylesheetInIframe(iframeElement, stylesheetUrl) {
-            iframeElement.onload = function() {
-                var iframeDocument = iframeElement.contentDocument || iframeElement.contentWindow.document;
-                console.log(iframeDocument);
-                // Create a new <link> element for the stylesheet
-                var linkElement = iframeDocument.createElement("link");
-                linkElement.rel = "stylesheet";
-                linkElement.type = "text/css"
-                linkElement.href = stylesheetUrl;
-                
-                // Append the link element to the iframe's head
-                iframeDocument.head.appendChild(linkElement);
-            };
-        }
-
-        // Call the function to load the additional stylesheet
-        var iframe = document.getElementById("mybbCanvas");
-        loadStylesheetInIframe(iframe, "https://github.com/mattmcnee/uom-restyle-extension/blob/main/content.css");
 
 
 
 
 
-// function checkValue() {
-//   console.log(iframe.contentWindow.innerWidth);
-//   if (iframe.contentWindow.innerWidth > 1) {
-//     console.log(iframe.contentDocument);
-//   } else {
-//     setTimeout(checkValue, 100);
-//   }
-// }
-
-// function addStylesheetToIframe(iframe, stylesheetUrl) {
-//     console.log(iframe.contentWindow);
-//     checkValue();
-//     // iframe.contentWindow.addEventListener('load', function() {
-//         console.log('Custom script injected');
-//         var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-//         console.log(iframeDocument);
-//         var linkElement = iframeDocument.createElement('link');
-//         linkElement.rel = 'stylesheet';
-//         linkElement.href = stylesheetUrl;
-//         iframeDocument.head.appendChild(linkElement);
-//     // });
-// }
-
-// // Usage example
-// var iframe = document.getElementById('mybbCanvas');
-// var stylesheetUrl = 'content.css';
-// addStylesheetToIframe(iframe, stylesheetUrl);
+addInlineStylesInIframe(iframe, inlineCSS);
 
 
